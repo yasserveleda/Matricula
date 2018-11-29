cfacf6dd556372b639f91c4585986d035faa71b1
yasserveleda <veledinha-a@hotmail.com>
2018-11-29 21:52:42 -0200

    adicionando lista de disciplinas oferecidas e matriculadas

--- Frontend/matricula/src/app/matricula/matricula/matricula.component.ts
+++ Frontend/matricula/src/app/matricula/matricula/matricula.component.ts
@@ -10,13 +10,14 @@ declare let $ :any;
 })
 export class MatriculaComponent implements OnInit {
 
-  private disciplinas: any[];
-  private historicoAluno: any[];
+  disciplinas: any[];
+  historicoAluno: any[];
   disciplinasOferecidas: any[];
-
-  private exibirErro: boolean;
-  private aluno;
-  private matricula: string;
+  disciplinasMatriculadas: any[];
+  disciplinasMatriculadasId: any[];
+  exibirErro: boolean;
+  aluno;
+  matricula: string;
 
   constructor(private matriculaservice: MatriculaSerivce) { 
     this.aluno = {};
@@ -33,26 +34,45 @@ export class MatriculaComponent implements OnInit {
   matriculaAluno() {
     if(this.aluno.id) {
       this.matriculaservice.matriculaAluno(this.aluno).subscribe( data => {
-        console.log(data);
+        
+        if(data[`id`]) {
+          this.aluno = data;
+          this.disciplinasMatriculadasId = this.aluno.fields.DISCIPLINAS_MATRICULADO;
+          this.listaDisciplinasMatriculadas();
+        }
+
       });
     }
   }
 
   //Faz a busca do Aluno atraves da matricula
-  getAlunoHistorico() {
+  getAluno() {
     let matricula = this.matricula;
 
     this.matriculaservice.getAluno(matricula).subscribe( data => {
       if(data[`records`].length > 0){
         this.aluno = data[`records`][0];
+        this.disciplinasMatriculadasId = this.aluno.fields.DISCIPLINAS_MATRICULADO;
+        this.listaDisciplinasMatriculadas();
+
+        this.getHistorico();
       }
     });
+  }
+
+  getHistorico() {
+    let matricula = this.matricula;
 
     this.matriculaservice.getHistorico(matricula).subscribe( data => {
       if(data[`records`].length > 0){
         this.exibirErro = false;
         this.historicoAluno = data[`records`];
-        this.listarDisciplinasOferecidas(this.historicoAluno);
+        //copia das disciplinas que o aluno está matriculado
+        let auxDisciplinasMatriculadas = this.disciplinasMatriculadas;
+        //disciplinas que serao removidas das oferecidas
+        let disciplinasRemoviveis = [];
+        disciplinasRemoviveis = auxDisciplinasMatriculadas.concat(this.historicoAluno);
+        this.listarDisciplinasOferecidas(disciplinasRemoviveis);
       } else {
         this.exibirErro = true;
         this.historicoAluno = [];
@@ -64,6 +84,7 @@ export class MatriculaComponent implements OnInit {
   //Lista as Disciplinas Oferecidas para o Aluno Buscado
   listarDisciplinasOferecidas(disciplinasHistorico) {
     let todasDisciplinas = this.disciplinas;
+
     for(let j = 0; j < disciplinasHistorico.length; j++) {
       for( var i = 0; i < todasDisciplinas.length; i++){ 
         if ( todasDisciplinas[i]['fields'].CODCRED == disciplinasHistorico[j]['fields'].CODCRED) {
@@ -71,9 +92,24 @@ export class MatriculaComponent implements OnInit {
         }
       }
     }
+    
     this.disciplinasOferecidas = todasDisciplinas;
   }
 
+  listaDisciplinasMatriculadas() {
+    let todasDisciplinas = this.disciplinas;
+    let listaGuardaMatriculadas = [];
+
+    for(let j = 0; j < this.disciplinasMatriculadasId.length; j++) {
+      for( var i = 0; i < todasDisciplinas.length; i++){ 
+        if ( todasDisciplinas[i]['id'] == this.disciplinasMatriculadasId[j]) {
+          listaGuardaMatriculadas.push(todasDisciplinas[i]);
+        }
+      }
+    }
+
+    this.disciplinasMatriculadas = listaGuardaMatriculadas;
+  }
   
   matriculaDisciplinaAluno(disciplina) {
     if(this.aluno.fields.DISCIPLINAS_MATRICULADO) {
--- Frontend/matricula/src/app/matricula/matricula/matricula.service.ts
+++ Frontend/matricula/src/app/matricula/matricula/matricula.service.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 
 interface myData {
+    status: string
     obj: Object
 }
 
@@ -46,4 +47,5 @@ export class MatriculaSerivce {
 
         return this.http.patch<myData>(url, alunoAux, this.httpOptions);
     }
+
 }
